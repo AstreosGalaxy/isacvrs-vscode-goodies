2989fa68b8bd7da2d78dcfcb2164c04f06b11b0f
IsacVR <159596961+IsacVR@users.noreply.github.com>
2024-08-05 19:12:15 -0400

    woah

---
+++ furretwalk.py
@@ -0,0 +1,3 @@
+from webbrowser import *
+while True:
+    open("https://youtu.be/ih9zBLDr_ro?si=cr4TYmxREzPr9LYY")
--- shikanokonokokoshtantan.py
+++ shikanokonokokoshtantan.py
@@ -1,1 +1,3 @@
-
+from webbrowser import *
+while True:
+    open("https://youtu.be/nwuW98yLsgY?si=uhpz1f-4DPtIql8Q")
